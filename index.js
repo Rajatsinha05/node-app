a4142cef3d2ff7a53166ea0aa8ec14298b3b7463
rajatsinha05 <rajatsinha5467@gmail.com>
2025-03-11 17:29:33 +0530

    aws

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const express=require("express")
-const app=express()
-app.use(express.json())
-app.get("/",(req,res)=>{
-    res.json({
-        msg:"working"
-    })
-})
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    msg: "working",
+  });
+});
 
-app.get("/test",(req,res)=>{
-    res.send("testing")
-})
-app.post("/",(req,res)=>{
-    res.send(req.body)
-})
-app.listen(8090,()=>{
-    console.log("running");
-    
-})
+app.get("/test", (req, res) => {
+  res.send("testing");
+});
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+app.get("/aws", (req, res) => {
+  res.send({
+    msg: "aws",
+  });
+});
+app.listen(8090, () => {
+  console.log("listening on port 8090");
+});
